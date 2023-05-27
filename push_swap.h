7f1ab61cdc4b040bc5d574cf3612fdcbcef123d3
Sam Acer <sam@sam.fr>
2023-05-27 10:33:20 +0200

    Makefile en béton armée

--- push_swap.h
+++ push_swap.h
@@ -6,14 +6,14 @@
 /*   By: snaggara <snaggara@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/11 18:15:57 by snaggara          #+#    #+#             */
-/*   Updated: 2023/05/26 15:05:03 by snaggara         ###   ########.fr       */
+/*   Updated: 2023/05/26 19:08:38 by snaggara         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
-# include "libs/ft_printf.h"
-# include "libs/libft.h"
+# include "printf/ft_printf.h"
+# include "printf/libft.h"
 # include <stdarg.h>
 # include <string.h>
 
